dd851cf0abf17d5573ba4a414f7d569d0acfe809
Chang Li <lichang@oregonstate.edu>
2018-12-07 16:25:09 +0800

    init the infoWindow on MP, unstable, don't use it.

--- pages/show_gallery/show_gallery.js
+++ pages/show_gallery/show_gallery.js
@@ -2,13 +2,45 @@
 let app = getApp()
 
 Page({
+  data: {
+    items: [
+      { name: '', value: '' }
+    ]
+  },
+  showModal: function (e) {
+    var showName = e.currentTarget.dataset.modal;
+    this.setData({
+      modalName: showName
+    })
+  },
+  closeModal: function (e) {
+    this.setData({
+      modalName: null
+    })
+  },
 
   data: {
 
   },
 
+  activeShowModal: function (e) {
+    console.log(3333,e)
+    const id = e.currentTarget.dataset.id
+    const title = e.currentTarget.dataset.title
+    const description = e.currentTarget.dataset.description
+    console.log(description)
+    console.log(`Click at item id : ${id}`)
+    
+    wx.showModal({
+      title: `${title}`,
+      content: `${description}`,
+    })
+  },
+
   onLoad: function (options) {
     let page = this;
+
+    
 
     //Request API to get workspace
     wx.request({
